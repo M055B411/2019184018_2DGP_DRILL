baa41f1c41f2661779b0d5f31aa29336ea16b1bf
JaeSung_Yang <imossball@gmail.com>
2022-09-18 15:37:18 +0900

    Drill#4 added
    Drill#4 완성

---
+++ DRILL#4/drill4.py
@@ -0,0 +1,31 @@
+import turtle
+
+sqare = 4
+while sqare > 0:
+    turtle.forward(500)
+    turtle.left(90)
+    sqare -= 1
+
+vert = 4
+while vert > 0:
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(500)
+    turtle.backward(500)
+    turtle.right(90)
+    vert -= 1
+
+turtle.forward(100)
+turtle.left(90)
+
+hori = 4
+while hori > 0:
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(500)
+    turtle.backward(500)
+    turtle.right(90)
+    hori -= 1
+
+turtle.forward(100)
+turtle.right(90)
